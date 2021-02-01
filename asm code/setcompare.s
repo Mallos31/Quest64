91a4d4a49b8425e92bd54be0b8a1c7887fd5f8b9
Mallos31 <Mallos42@gmail.com>
2021-02-01 16:37:40 -0600

    Update and rename code_488D0.s to setcompare.s

--- asm code/setcompare.s
+++ asm code/setcompare.s
@@ -3,3 +3,4 @@ glabel func_80047CD0
 /* 488D4 80047CD4 03E00008 */  jr         $ra
 /* 488D8 80047CD8 00000000 */   nop       
 /* 488DC 80047CDC 00000000 */  nop        
+/*This function is handwritten and uses 'invalid' asm. Cannot be decompiled*/
